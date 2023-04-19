82aa11811d2cb40de4b02b35323ea0a8817b0892
EmersonGarrido <emersongarrido@hotmail.com.br>
2023-04-19 16:38:41 -0400

    initial project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,105 +1,27 @@
-"use client";
 import * as React from "react";
 import { Roboto } from "next/font/google";
 import Image from "next/image";
-import { FaWhatsapp, FaMapMarkedAlt } from "react-icons/fa";
 import { MdVerified } from "react-icons/md";
 import { HiShoppingCart } from "react-icons/hi";
-import { HiCursorClick } from "react-icons/hi";
+import Link from "next/link";
+
+import { Content } from "components";
 
 const roboto = Roboto({
   weight: ["100", "400", "500", "700", "900"],
   subsets: ["latin"],
 });
 
-const lojas = [
-  {
-    id: 1,
-    cover: "carioca-leblon.png",
-    name: "Carioca Jeans Leblon",
-    address: "R. Clineu da Costa Morães, 627 - Jardim Leblon",
-    map: "https://goo.gl/maps/1NWbgXWJjH9mPBDEA",
-    contact: [
-      {
-        name: "Emerson Garrido",
-        number: "67991355709",
-      },
-      {
-        name: "Whatsapp 2",
-        number: "67993020672",
-      },
-    ],
-    open: false,
-  },
-  {
-    id: 2,
-    cover: "cover-leblon.png",
-    name: "Carioca Jeans Monte Castelo",
-    address: "Av. Mascarenhas de Moraes, 1826 - Monte Castelo",
-    map: "https://goo.gl/maps/NZgKpWERyigCQw447",
-    contact: [
-      {
-        name: "Whatsapp 1",
-        number: "67991179242",
-      },
-      {
-        name: "Whatsapp 2",
-        number: "67992137114",
-      },
-    ],
-    open: false,
-  },
-  {
-    id: 3,
-    cover: "cover-leblon.png",
-    name: "Carioca Jeans Aero Rancho",
-    address: "Av. Ezequiel Ferreira Lima, 1228 - Loja 1 - Jardim Aero Rancho",
-    map: "https://goo.gl/maps/P4K1DHBmUFM5w6sx9",
-    contact: [
-      {
-        name: "Whatsapp 1",
-        number: "67992151555",
-      },
-      {
-        name: "Whatsapp 2",
-        number: "67992632717",
-      },
-    ],
-    open: false,
-  },
-  {
-    id: 4,
-    cover: "cover-leblon.png",
-    name: "Carioca Jeans Tiradentes",
-    address: "Av. Ezequiel Ferreira Lima, 1228 - Loja 1 - Jardim Aero Rancho",
-    map: "https://goo.gl/maps/P4K1DHBmUFM5w6sx9",
-    contact: [
-      {
-        name: "Whatsapp 1",
-        number: "67992151555",
-      },
-      {
-        name: "Whatsapp 2",
-        number: "67992632717",
-      },
-    ],
-    open: false,
-  },
-];
+async function getData() {
+  const res = await fetch(
+    "http://n8n.emersongarrido.com.br:5678/webhook/10baea80-222a-480f-b1e6-fe2623e527c4",
+    { next: { revalidate: 30 } }
+  );
+  return res.json();
+}
 
-export default function Home() {
-  const [state, setState] = React.useState(lojas);
-
-  function handleOpenInfo(data: any) {
-    const newState = [...state];
-
-    const item = newState.find((isState) => isState.id === data.id);
-
-    if (item) {
-      item.open = !data.open;
-      setState(newState);
-    }
-  }
+export default async function Home() {
+  const data = await getData();
 
   return (
     <main
@@ -112,84 +34,17 @@ export default function Home() {
         @cariocajeans <MdVerified color="#1a40a3" size={18} />
       </h1>
 
-      <button
-        onClick={() => window.open("https://cariocajeans.com.br", "blank")}
-        className="flex gap-3 w-full max-w-[340px] items-center justify-center text-base mt-5 mb-3 bg-[#1a77f3] shadow-md p-4 rounded-md text-white"
-      >
-        <HiShoppingCart size={20} /> Compre na Loja Online
-      </button>
+      <Link href="https://cariocajeans.com.br">
+        <button className="flex gap-3 w-full max-w-[340px] items-center justify-center text-base mt-5 mb-3 bg-[#1a77f3] shadow-md p-4 rounded-md text-white">
+          <HiShoppingCart size={20} /> Compre na Loja Online
+        </button>
+      </Link>
 
       <h1 className="font-thin text-xl mt-3 mb-3">
         ou Escolha uma Lojas Fisícas
       </h1>
-      <div className="mt-5 flex flex-col gap-5 w-full items-center justify-center max-w-[340px]">
-        {state.map((loja, index) => {
-          return (
-            <div
-              key={index}
-              className="border-black/5 relative w-full max-w-[340px] bg-white shadow-sm flex flex-col items-center justify-center gap-3 border-[0.1rem] rounded-md"
-            >
-              <img
-                onClick={() => handleOpenInfo(loja)}
-                className="rounded-md contain w-full max-w-[340px] h-[160px] container backdrop-brightness-50"
-                src={`/${loja.cover}`}
-                alt={loja.name}
-              />
 
-              {loja.open === false && (
-                <div
-                  onClick={() => handleOpenInfo(loja)}
-                  className="absolute flex-col gap-3 w-full max-w-[340px] h-[160px] flex items-center justify-between p-2 bg-black/50 rounded-md"
-                >
-                  <div></div>
-                  <div className="flex items-center justify-center">
-                    <HiCursorClick
-                      color="#FFF"
-                      size={35}
-                      className="animate-pulse"
-                    />
-                    {/* <span className="font-bold text-white animate-pulse">
-                      clique aqui
-                    </span> */}
-                  </div>
-                  <h1 className="font-bold text-xl text-white">{loja.name}</h1>
-                </div>
-              )}
-
-              {loja.open && (
-                <div className="p-1 w-full flex items-center justify-center flex-col gap-3 max-w-[350px]">
-                  <h1 className="font-bold text-center text-lg">{loja.name}</h1>
-                  <button
-                    onClick={() => window.open(`${loja.map}`, "blank")}
-                    className="font-light flex items-center justify-center flex-col text-center text-sm"
-                  >
-                    <FaMapMarkedAlt color="#1a40a3" size={25} /> {loja.address}
-                  </button>
-
-                  <div className="flex items-center flex-col w-full p-2 justify-center">
-                    {loja.contact.map((contact, isIndex) => {
-                      return (
-                        <button
-                          key={isIndex}
-                          onClick={() =>
-                            window.open(
-                              `https://wa.me/55${contact.number}`,
-                              "blank"
-                            )
-                          }
-                          className="bg-[#128C7E] flex items-center text-base justify-center gap-3 text-white p-3 m-2 w-full rounded-md"
-                        >
-                          <FaWhatsapp size={25} /> {contact.name}
-                        </button>
-                      );
-                    })}
-                  </div>
-                </div>
-              )}
-            </div>
-          );
-        })}
-      </div>
+      <Content lojas={data} />
 
       <div className="text-xs text-center mt-5 mb-4">
         <span>
---
+++ src/components/content/index.tsx
@@ -0,0 +1,101 @@
+"use client";
+import * as React from "react";
+
+import { FaWhatsapp, FaMapMarkedAlt } from "react-icons/fa";
+import { HiCursorClick } from "react-icons/hi";
+
+interface ContentProp {
+  lojas: any[];
+}
+
+const Content: React.FC<ContentProp> = ({ lojas }) => {
+  const [state, setState] = React.useState<any>(lojas);
+
+  function handleOpenInfo(data: any, index: number) {
+    const newState = [...state];
+    const isOpen = newState[index].open;
+
+    if (isOpen) {
+      newState[index].open = false;
+      setState(newState);
+    } else {
+      newState[index].open = true;
+      setState(newState);
+    }
+  }
+  return (
+    <>
+      <div className="mt-5 flex flex-col gap-5 w-full items-center justify-center max-w-[340px]">
+        {state.map((loja: any, index: number) => {
+          return (
+            <div
+              key={index}
+              className="border-black/5 relative w-full max-w-[340px] bg-white shadow-sm flex flex-col items-center justify-center gap-3 border-[0.1rem] rounded-md"
+            >
+              <img
+                onClick={() => handleOpenInfo(loja, index)}
+                className="rounded-md contain w-full max-w-[340px] h-[160px] container backdrop-brightness-50"
+                src={`https://drive.google.com/uc?export=download&id=${loja.cover}`}
+                alt={loja.nome}
+              />
+
+              {loja?.open !== true && (
+                <div
+                  onClick={() => handleOpenInfo(loja, index)}
+                  className="absolute flex-col gap-3 w-full max-w-[340px] h-[160px] flex items-center justify-between p-2 bg-black/50 rounded-md"
+                >
+                  <div></div>
+                  <div className="flex items-center justify-center">
+                    <HiCursorClick
+                      color="#FFF"
+                      size={35}
+                      className="animate-pulse"
+                    />
+                    {/* <span className="font-bold text-white animate-pulse">
+                      clique aqui
+                    </span> */}
+                  </div>
+                  <h1 className="font-bold text-xl text-white">{loja.nome}</h1>
+                </div>
+              )}
+
+              {loja?.open && (
+                <div className="p-1 w-full flex items-center justify-center flex-col gap-3 max-w-[350px]">
+                  <h1 className="font-bold text-center text-lg">{loja.nome}</h1>
+                  <button
+                    onClick={() => window.open(`${loja.map}`, "blank")}
+                    className="font-light flex items-center justify-center flex-col text-center text-sm"
+                  >
+                    <FaMapMarkedAlt color="#1a40a3" size={25} /> {loja.endereco}
+                  </button>
+
+                  <div className="flex items-center flex-col w-full p-2 justify-center">
+                    <button
+                      onClick={() =>
+                        window.open(`https://wa.me/55${loja.numero_1}`, "blank")
+                      }
+                      className="bg-[#128C7E] flex items-center text-base justify-center gap-3 text-white p-3 m-2 w-full rounded-md"
+                    >
+                      <FaWhatsapp size={25} /> {loja.nome_1}
+                    </button>
+
+                    <button
+                      onClick={() =>
+                        window.open(`https://wa.me/55${loja.numero_2}`, "blank")
+                      }
+                      className="bg-[#128C7E] flex items-center text-base justify-center gap-3 text-white p-3 m-2 w-full rounded-md"
+                    >
+                      <FaWhatsapp size={25} /> {loja.nome_2}
+                    </button>
+                  </div>
+                </div>
+              )}
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
+};
+
+export default Content;
